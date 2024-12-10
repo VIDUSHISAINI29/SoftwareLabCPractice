4c684e60ffa1f4957dc680c545079ea033f9d318
VIDUSHISAINI29 <vidushisaini29@gmail.com>
2024-12-10 18:42:10 +0530

    multiplication of matrices

--- SLC6.C
+++ SLC6.C
@@ -1,14 +1,45 @@
 #include<stdio.h>
 #include<conio.h>
-void main()
+int main()
 	{
-		int arr1[5] = {2, 3, 4, 5, 6}, arr2[5] =  {2, 3, 4, 5, 6}, resultArr[5], i, j, k;
-		clrscr();
-		for(i = 0; i < 5; i++)
+		int arr1[2][3], arr2[3][2], resultArr[2][2] = {0};
+		int i, j, k = 0;
+		
+		for(i = 0; i < 2; i++)
 		{
-			resultArr[i] = arr1[i] * arr2[i];
-			 printf("In resulted array element at %d is %d\n", i, resultArr[i]);
+			for(j = 0; j < 3; j++)
+			{
+				printf("Enter %d_%d element for array 1 (2 * 3) : ", i + 1, j + 1);
+				scanf("%d", &arr1[i][j]);
+			}
 		}
+		for(i = 0; i < 3; i++)
+		{
+			for(j = 0; j < 2; j++)
+			{
+				printf("Enter %d_%d element for array 2 (3 * 2) : ", i + 1, j + 1);
+				scanf("%d", &arr2[i][j]);
+			}
+		}
+		for (i = 0; i < 2; i++)
+		{
+			for(j = 0; j < 2; j++)
+				{
+						resultArr[i][j] = 0;
+					for(k = 0; k < 3; k++)
+					{
+						resultArr[i][j] += arr1[i][k] * arr2[k][j];
+					}
+				}
+		}
+		for(i = 0; i < 2; i++)
+				{
+					for(j = 0; j < 2; j++)
+					{
+						
+						printf(" %d_%d  element of resultant array is : %d \n", i + 1, j + 1, resultArr[i][j]);
+					}
+				}
 
-		getch();
+		return 0;
 	}
