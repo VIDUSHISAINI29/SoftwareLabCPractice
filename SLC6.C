ade7665fa228e2cff941a4821aefe18ca36c0a92
VIDUSHISAINI29 <vidushisaini29@gmail.com>
2024-12-14 21:14:42 +0530

    more

--- SLC6.C
+++ SLC6.C
@@ -1,45 +1,79 @@
-#include<stdio.h>
-#include<conio.h>
-int main()
-	{
-		int arr1[2][3], arr2[3][2], resultArr[2][2] = {0};
-		int i, j, k = 0;
-		
-		for(i = 0; i < 2; i++)
-		{
-			for(j = 0; j < 3; j++)
-			{
-				printf("Enter %d_%d element for array 1 (2 * 3) : ", i + 1, j + 1);
-				scanf("%d", &arr1[i][j]);
-			}
-		}
-		for(i = 0; i < 3; i++)
-		{
-			for(j = 0; j < 2; j++)
-			{
-				printf("Enter %d_%d element for array 2 (3 * 2) : ", i + 1, j + 1);
-				scanf("%d", &arr2[i][j]);
-			}
-		}
-		for (i = 0; i < 2; i++)
-		{
-			for(j = 0; j < 2; j++)
-				{
-						resultArr[i][j] = 0;
-					for(k = 0; k < 3; k++)
-					{
-						resultArr[i][j] += arr1[i][k] * arr2[k][j];
-					}
-				}
-		}
-		for(i = 0; i < 2; i++)
-				{
-					for(j = 0; j < 2; j++)
-					{
-						
-						printf(" %d_%d  element of resultant array is : %d \n", i + 1, j + 1, resultArr[i][j]);
-					}
-				}
+// # Multiplication of matrices
 
-		return 0;
+// #include<stdio.h>
+// int main()
+// 	{
+// 		int arr1[2][3], arr2[3][2], resultArr[2][2] = {0};
+// 		int i, j, k = 0;
+		
+// 		for(i = 0; i < 2; i++)
+// 		{
+// 			for(j = 0; j < 3; j++)
+// 			{
+// 				printf("Enter %d_%d element for array 1 (2 * 3) : ", i + 1, j + 1);
+// 				scanf("%d", &arr1[i][j]);
+// 			}
+// 		}
+// 		for(i = 0; i < 3; i++)
+// 		{
+// 			for(j = 0; j < 2; j++)
+// 			{
+// 				printf("Enter %d_%d element for array 2 (3 * 2) : ", i + 1, j + 1);
+// 				scanf("%d", &arr2[i][j]);
+// 			}
+// 		}
+// 		for (i = 0; i < 2; i++)
+// 		{
+// 			for(j = 0; j < 2; j++)
+// 				{
+// 						resultArr[i][j] = 0;
+// 					for(k = 0; k < 3; k++)
+// 					{
+// 						resultArr[i][j] += arr1[i][k] * arr2[k][j];
+// 					}
+// 				}
+// 		}
+// 		for(i = 0; i < 2; i++)
+// 				{
+// 					for(j = 0; j < 2; j++)
+// 					{
+						
+// 						printf(" %d_%d  element of resultant array is : %d \n", i + 1, j + 1, resultArr[i][j]);
+// 					}
+// 				}
+
+// 		return 0;
+// 	}
+
+// # Multiplication of matrices
+
+#include<stdio.h>
+#define N 50
+
+void main(){
+	int row1, row2, col1, col2, matrix1[N], matrix2[N], i, j, k, resultantMatrix;
+	printf("Enter number of rows and coloumns of matrix 1 : ");
+	scanf("%d %d \n", &row1, &col1);
+	for ( i = 0; i < row1; i++)
+	{
+		for ( j = 0; j < col1; j++)
+		{
+			printf("Enter %d_%d element of matrix 1 : \t", i, j);
+			scanf("%d", &matrix1[i][j]);
+		}
 	}
+	printf("Enter number of rows and coloumns of matrix 2 : ");
+	scanf("%d %d \n", &row2, &col2);
+	for ( i = 0; i < row2; i++)
+	{
+		for ( j = 0; j < col2; j++)
+		{
+			printf("Enter %d_%d element of matrix 2 : \t", i, j);
+			scanf("%d", &matrix2[i][j]);
+		}
+	}
+	if(col1 != row2){
+		printf("cannot multiply")
+	}
+	
+}
--- first.c
+++ first.c
@@ -1,28 +1,65 @@
-#include<stdio.h>
-#include<math.h>
+// #include<stdio.h>
+// #include<math.h>
+// int main(){
+//     int rows, i, j, k;
+//     printf("Enter Number of Rows : ");
+//     scanf("%d", &rows);
+
+//     for(i = 1; i <= rows; i++){
+//         for(j = 1; j <= (rows - i) ; j++){
+//             printf(" ");
+//         }
+//         for(k = 1; k <= (i * 2 - 1) ; k++){
+//             printf("%d", i);
+//         }
+//         printf("\n");
+//     }
+
+//        for(i = 0; i <= rows; i++){
+//         for(j = 1; j <= i ; j++){
+//             printf(" ");
+//         }
+//         for(k = 1; k <= 2 * (rows - i) - 1 ; k++){
+//             printf("%d",k);
+//         }
+//         printf("\n");
+//         }
+
+// }
+
+
+//# Factorial Program
+
+
+// #include<stdio.h>
+// int factorial(int n){
+//     int i, fact = 1;
+//     for(int i = 1; i <= n; i++){
+//         fact = fact * i ;
+     
+//     }
+//     return fact;
+// }
+// int main(){
+//     int num ;
+//     printf("Enter a number of which factorial you want : ");
+//     scanf("%d", &num);
+//     printf("Factorial of %d is %d \n", num, factorial(num));
+//     return 0;
+// }
+
+//# Swapping two numbers using pointers
+
+void swapping (int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 int main(){
-    int rows, i, j, k;
-    printf("Enter Number of Rows : ");
-    scanf("%d", &rows);
-
-    for(i = 1; i <= rows; i++){
-        for(j = 1; j <= (rows - i) ; j++){
-            printf(" ");
-        }
-        for(k = 1; k <= (i * 2 - 1) ; k++){
-            printf("%d", i);
-        }
-        printf("\n");
-    }
-
-    //    for(i = 0; i <= rows; i++){
-    //     for(j = 1; j <= i ; j++){
-    //         printf(" ");
-    //     }
-    //     for(k = 1; k <= 2 * (rows - i) - 1 ; k++){
-    //         printf("%d",k);
-    //     }
-    //     printf("\n");
-    //     }
-
+    int a = 10, b = 15;
+    int *ptr1 = &a, *ptr2 = &b;
+    printf("Before swapping : a = %d, b = %d\n", a, b);
+    swapping(ptr1, ptr2);
+    printf("After swapping : a = %d, b = %d\n", a, b);
+    return 0;
 }
